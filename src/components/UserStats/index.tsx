da4614ac7f74220bd3378b85d0ddaa56a1e16134
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-02-08 21:23:26 -0300

    criou pagina de estastistica utilizando o Victory JS

--- src/components/UserStats/index.tsx
+++ src/components/UserStats/index.tsx
@@ -1,13 +1,34 @@
-import Head from '../Head';
+import { lazy, Suspense, useEffect } from 'react';
 
-import styles from './styles.module.css'
+import Head from '../Head';
+import Loading from '../Loading';
+import Error from '../Error';
+import useFetch from '../../hooks/useFetch';
+import { STATS_GET } from '../../api';
+const UserStatsGraph = lazy(() => import('../UserStatsGraph'))
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Estatísticas' description="Pagina de estatísticas" />
-    </div>
-  )
+  const { data, error, loading, request } = useFetch()
+
+  useEffect(() => {
+    const getData = async () => {
+      const { url, options } = STATS_GET()
+      await request(url, options)
+    }
+    getData()
+  }, [request])
+
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
+  if (data)
+
+    return (
+      <Suspense fallback={<div></div>}>
+        <Head title='Estatísticas' description="Pagina de estatísticas" />
+        <UserStatsGraph data={data} />
+      </Suspense>
+    )
+  else return null
 };
 
 export default UserStats;
